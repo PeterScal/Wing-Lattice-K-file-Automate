273ec353f14fa071ecf310175394058c404b6448
PeterScal <peterscal26@gmail.com>
2026-01-05 10:20:58 -0500

    updated autoMATs

--- Dyna_AutoMATs.py
+++ Dyna_AutoMATs.py
@@ -1,11 +1,15 @@
 from ansys.dyna.core import Deck
 from ansys.dyna.core import keywords as kwd
+import lsdyna_mesh_reader as mesh
 import os
+import numpy as np
 
 # Define file paths
 Current_Directory = os.path.dirname(os.path.abspath(__file__))
-Input_LatticeFile = os.path.join(Current_Directory, "WingLattice_v2.k")
-Output_LatticeFile = os.path.join(Current_Directory, "WingLattice_v2_updated.k")
+Ls_Dyna_Folder = os.path.dirname(Current_Directory)
+
+# Path to the file containing the NODE definitions
+Input_LatticeFile = os.path.join(Ls_Dyna_Folder,"LS-DYNA", "WingLattice_v2.k") 
 
 # Load your existing mesh
 deck = Deck(Input_LatticeFile)
@@ -17,19 +21,93 @@ mat.ro = 1.25e-9
 mat.e = 1.0
 mat.sigy = 0.05
 mat.pr = 0.35
+mat.etan = 0.2
 
-# Create the Section keyword (SID 1)
-sec = kwd.SectionSolid(secid=1)
+# Create the Section keyword (SECID 3)
+sec = kwd.SectionSolid(secid=3)
 sec.elform = 10 # Constant Stress Solid
 
-# Create/Update the Part keyword to link them
+# Create the Part keyword (PID 1) linking to the Material and Section   
 part = kwd.Part(pid=1, mid=mat.mid, secid=sec.secid)
 
+#--------------------------------------------------------------------------------------------------------------------------------------
+# New Deck using mesh reader to find nodes on surface planes
+mesh_deck = mesh.Deck(Input_LatticeFile)
+node_section = mesh_deck.node_sections[0]
+print(node_section)
 
+# Test reading .k file
+print(node_section.coordinates)
+print(node_section.nid)
 
+# Configuration for the surface plane for faom to lattice connection
+x_target = 76.0
+y_min, y_max = -3.897, 17.392
+z_min, z_max = 0.1307, 101.554
+tolerance = 0.1 
 
-# Append these keywords to the deck and write the file
-deck.extend([mat, sec, part])
+# Access coordinates and NIDs from the reader
+coords = node_section.coordinates
+nids = node_section.nid
+
+# Create booleans for each dimension based on your targets
+nodes_x = (coords[:, 0] >= x_target - tolerance) & (coords[:, 0] <= x_target + tolerance)
+nodes_y = (coords[:, 1] >= y_min) & (coords[:, 1] <= y_max)
+nodes_z = (coords[:, 2] >= z_min) & (coords[:, 2] <= z_max)
+
+# Combine booleans to find nodes satisfying all criteria
+full_nodes = nodes_x & nodes_y & nodes_z
+
+# Extract the resulting Node IDs
+target_node_ids_latfoam = nids[full_nodes].tolist()
+
+# List nodes found and used for the node set
+print(f"Found {len(target_node_ids_latfoam)} nodes.")
+print(target_node_ids_latfoam)
+
+#--------------------------------------------------------------------------------------------------------------------------------------
+# Configuration for the surface plane for lattice to UAV Body connection
+x_max = 76.102
+x_min = 0.047
+z_target = 0.3024
+y_min = -5.328
+y_max = 17.908
+
+# Create booleans for each dimension based on your targets
+nodes_z = (coords[:, 2] >= z_target - tolerance) & (coords[:, 2] <= z_target + tolerance)
+nodes_y = (coords[:, 1] >= y_min) & (coords[:, 1] <= y_max)
+nodes_x = (coords[:, 0] >= x_min) & (coords[:, 0] <= x_max)
+
+# Combine booleans to find nodes satisfying all criteria
+full_nodes = nodes_x & nodes_y & nodes_z
+
+# Extract the resulting Node IDs
+target_node_ids_latUAV = nids[full_nodes].tolist()
+
+# List nodes found and used for the node set
+print(f"Found {len(target_node_ids_latUAV)} nodes.")
+print(target_node_ids_latUAV)
+
+#--------------------------------------------------------------------------------------------------------------------------------------
+# Explicitly delete the objects holding file handles
+del mesh_deck
+del node_section
+
+# Add new node set to deck:
+node_set_foam = kwd.SetNodeList(sid=1)
+node_set_foam.nodes = target_node_ids_latfoam
+
+# Add new node set to deck:
+node_set_uav = kwd.SetNodeList(sid=2)
+node_set_uav.nodes = target_node_ids_latUAV
+
+# Extend deck with new keywords
+
+if deck.get_kwds_by_type('MATERIAL') is None and deck.get_kwds_by_type('SECTION') is None and deck.get_kwds_by_type('SET') is None:
+    deck.extend([mat, sec, part, node_set_foam, node_set_uav])
+else:
+    print('Material, Section, or Set keywords already exist in the deck. Check keywoprd notepad.')
+    SystemExit
 
 # Create LS-DYNA input deck
 deck_string = deck.write()
